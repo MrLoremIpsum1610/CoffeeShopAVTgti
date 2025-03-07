48a38b441cc5277fd6d12dd17960c256ab64de20
DEV-ThunderIpsum1610 <aalimohamad.allahyari@gmail.com>
2025-03-07 15:42:17 +0330

    Init.. of CoffeeShop Project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Dana-ExtraLight': '"Dana ExtraLight"',
+        'Dana-Light': '"Dana Light"',
+        'Dana-Regular': '"Dana Regular"',
+        'Dana-Medium': '"Dana Medium"',
+        'Dana-DemiBold': '"Dana DemiBold"',
+        'Dana-Bold': '"Dana Bold"',
+        'Dana-ExtraBold': '"Dana ExtraBold"',
+        'Dana-Black': '"Dana Black"',
+      }
+
+
+    },
+  },
+  plugins: [],
+}
+
